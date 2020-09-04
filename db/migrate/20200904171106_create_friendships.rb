e413c11d77a1bade6335b496512ecf5f77344cdc
gallardo <aiki.hosogaya@gmail.com>
2020-09-05 02:16:52 +0900

    Add friendships table

---
+++ db/migrate/20200904171106_create_friendships.rb
@@ -0,0 +1,14 @@
+class CreateFriendships < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friendships do |t|
+      t.integer :follower_id
+      t.integer :following_id
+
+      t.timestamps null: false
+    end
+
+    add_index :friendships, :follower_id
+    add_index :friendships, :following_id
+    add_index :friendships, [:follower_id, :following_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_135950) do
+ActiveRecord::Schema.define(version: 2020_09_04_171106) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 2020_09_01_135950) do
     t.string "picture"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id", "following_id"], name: "index_friendships_on_follower_id_and_following_id", unique: true
+    t.index ["follower_id"], name: "index_friendships_on_follower_id"
+    t.index ["following_id"], name: "index_friendships_on_following_id"
   end
 
 # Could not dump table "users" because of following StandardError
