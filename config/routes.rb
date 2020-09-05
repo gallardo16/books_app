62f9618755763243d2f2d0a65c0cb9652d0f84d3
gallardo <aiki.hosogaya@gmail.com>
2020-09-05 22:51:14 +0900

    Add controller and action and routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :books
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show] do
+    resource :friendships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
 end
