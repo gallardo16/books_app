854723e8d8a0ea296bc82afa685b728b5cf22847
gallardo <aiki.hosogaya@gmail.com>
2020-09-05 19:12:18 +0900

    Associate user model with friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :avatar
 
+  has_many :active_friendships, foreign_key: "following_id", class_name: "Friendship", dependent: :destroy
+  has_many :followings, through: :active_friendships, source: :followed
+  has_many :passive_friendships, foreign_key: "followed_id", class_name: "Friendship", dependent: :destroy
+  has_many :followers, through: :passive_friendships, source: :following
+
   def self.find_for_github_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.name = auth.info.name
@@ -21,5 +26,17 @@ class User < ApplicationRecord
 
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def follow(other_user)
+    active_friendships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_friendships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
   end
 end
