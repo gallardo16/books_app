15dc59ad478e34ff1a359ae2c8b8d425639817ad
gallardo <aiki.hosogaya@gmail.com>
2020-09-08 15:05:55 +0900

    Add reports post function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reports
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :books
