d6ce586cfbea4c44238b88a02437ce510de3799f
gallardo <aiki.hosogaya@gmail.com>
2020-09-15 16:02:09 +0900

    Modify migration

--- db/migrate/20200820144953_add_details_to_users.rb
+++ db/migrate/20200820144953_add_details_to_users.rb
@@ -1,6 +1,6 @@
 class AddDetailsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :name, :sting
+    add_column :users, :name, :string
     add_column :users, :postalcode, :integer
     add_column :users, :address, :string
     add_column :users, :biography, :text
