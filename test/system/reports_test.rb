c3f1638b1ab16b6c9e9597553c2a3064943ee1e5
gallardo <aiki.hosogaya@gmail.com>
2020-09-19 17:21:59 +0900

    Add report system test

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -1,45 +1,53 @@
 require "application_system_test_case"
 
 class ReportsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
   setup do
-    @report = reports(:one)
+    @report = reports(:report_1)
+    @user = users(:user_2)
+    sign_in @user
   end
 
-  test "visiting the index" do
-    visit reports_url
-    assert_selector "h1", text: "Reports"
+  test '日報一覧を表示する' do
+    visit reports_path
+    assert_selector 'h1', text: '日報一覧'
   end
 
-  test "creating a Report" do
-    visit reports_url
-    click_on "New Report"
+  test '日報を作成する' do
+    visit reports_path
+    click_on '新規作成'
 
-    fill_in "Content", with: @report.content
-    fill_in "Title", with: @report.title
-    click_on "Create Report"
+    fill_in '題名', with: @report.title
+    fill_in '内容', with: @report.content
+    click_on '登録する'
 
-    assert_text "Report was successfully created"
-    click_on "Back"
+    assert_text '作成しました。'
   end
 
-  test "updating a Report" do
-    visit reports_url
-    click_on "Edit", match: :first
-
-    fill_in "Content", with: @report.content
-    fill_in "Title", with: @report.title
-    click_on "Update Report"
-
-    assert_text "Report was successfully updated"
-    click_on "Back"
+  test '日報の詳細を表示する' do
+    visit report_path(@report)
+    assert_selector 'h1', text: '日報詳細'
+    assert_text 'report1_title'
+    assert_text 'report1_content'
   end
 
-  test "destroying a Report" do
-    visit reports_url
+  test '日報を更新する' do
+    visit reports_path
+    click_on '編集', match: :first
+
+    fill_in '題名', with: 'update_report1_title'
+    fill_in '内容', with: 'update_report1_content'
+    click_on '更新する'
+
+    assert_text '更新しました。'
+  end
+
+  test '日報を削除する' do
+    visit reports_path
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on '削除', match: :first
     end
 
-    assert_text "Report was successfully destroyed"
+    assert_text '削除しました。'
   end
 end
